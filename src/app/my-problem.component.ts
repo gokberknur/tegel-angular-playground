1eb85370c8e3632a78d86a546ccb64ada865e668
gokberknur <gokberknur@gmail.com>
2024-06-03 09:34:40 +0200

    adding custom elements

--- src/app/my-problem.component.ts
+++ src/app/my-problem.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, Component } from '@angular/core';
 
 @Component({
   selector: 'app-my-problem',
   standalone: true,
   imports: [],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   templateUrl: './my-problem.component.html',
   styleUrl: './my-problem.component.css'
 })
